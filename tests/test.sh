3708289417ad0307b65e05024b1b02e7513f5ddc
krushik <krushik@gmail.com>
2025-04-05 06:16:06 +0500

    test: Add test script to run pre-commit with arguments

---
+++ tests/test.sh
@@ -0,0 +1,18 @@
+# Determine pre-commit arguments based on script arguments
+if [ $# -eq 0 ]; then
+    PRE_COMMIT_ARGS="--all-files"
+else
+    PRE_COMMIT_ARGS="--files $*"
+fi
+
+# Run pre-commit, capture output, and allow for a second run if the first modifies files
+output=$(pre-commit run $PRE_COMMIT_ARGS)
+exit_code=$?
+
+if [ $exit_code -eq 0 ]; then
+    # First run succeeded, print its output
+    echo "$output"
+else
+    # First run failed (likely modified files), run again and show output
+    pre-commit run $PRE_COMMIT_ARGS
+fi
